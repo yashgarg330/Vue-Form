531899d1b05eb9ff35f965ba54e665aec4c8b738
yashgarg330 <56553174+yashgarg330@users.noreply.github.com>
2021-04-23 22:13:20 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,97 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <form>
+      <div class="row">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" v-model="form.name" />
+        <span> <button class="btn">Copy</button> </span>
+      </div>
+      <div class="row">
+        <label for="age">Age</label>
+        <input type="number" name="age" id="age" v-model="form.age" />
+      </div>
+      <div class="row">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" v-model="form.email" />
+      </div>
+      <div class="row">
+        <input v-on:click="postData" type="submit" value="Submit" />
+      </div>
+    </form>
   </div>
+
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "Form",
+  data() {
+    return {
+      form: {
+        name: "",
+        age: null,
+        email: "",
+      },
+    };
+  },
+  methods: {
+    postData(e) {
+      e.preventDefault()
+      console.log("My data", this.form);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+* {
+  margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 4px;
 }
-a {
-  color: #42b983;
+.row {
+  padding: 5px 0px;
+}
+label {
+  margin-right: 10px;
+}
+input {
+  background-color: skyblue;
+  padding: 2px 7px;
+}
+input[type="text"] {
+  margin-right: 5px;
+}
+input[type="number"] {
+  margin-left: 11px;
+  width: 100px;
+}
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+input[type="email"] {
+  margin-bottom: 5px;
+  width: 200px;
+}
+input[type="submit"] {
+  margin-left: 51px;
+  cursor: pointer;
+}
+.btn {
+  background-color: white;
+  border: 0;
+  cursor: pointer;
 }
 </style>
